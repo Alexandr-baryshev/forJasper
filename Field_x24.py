5b8925090c150573f8749150e7916488b659a99b
Alexandr <alexandr.baryshev@hotmail.com>
2019-06-06 13:59:00 +1000

    Просто SAVE

--- Field_x24.py
+++ Field_x24.py
@@ -4,8 +4,8 @@ import re
 
 bas_template = '''
 			<textField isBlankWhenNull="true">
-				<reportElement style="ДанныеПОДСтолбцов3" x="550" y="0" width="55" height="15" uuid="a2680c9e-d00d-49cd-9458-a27949d8e024"/>
-				<textFieldExpression><![CDATA[((long)($V{Возврат_0} / 3600)) + ":" + ((long)(($V{Возврат_0} % 3600)/60)) + ":" +((long)($V{Возврат_0} % 3600)%60 )]]></textFieldExpression>
+				<reportElement style="ДанныеПОДСтолбцов3" x="645" y="0" width="40" height="15" uuid="34258d57-bcc3-497a-be43-82709e777cd4"/>
+				<textFieldExpression><![CDATA[((long)($V{Станция_0} / 3600)) + ":" + ((long)(($V{Станция_0} % 3600)/60)) + ":" +((long)($V{Станция_0} % 3600)%60 )]]></textFieldExpression>
 			</textField>
  '''
 
--- Var_x24х3_Sequence.py
+++ Var_x24х3_Sequence.py
@@ -7,9 +7,9 @@ all_gotovo = ""
 #Первая серия переменных +
 
 bas_template = '''
-	<variable name="БригадаСумВремя(sec)_0" class="java.lang.Long" calculation="Sum">
-		<variableExpression><![CDATA[$V{Бригада_BooL} && $V{BooL_0}  ?
-( $F{Время возврата на базу}.getTime() - $F{Время выезда}.getTime() ) / 1000 : null]]></variableExpression>
+	<variable name="СтанцияСумВремя(sec)_0" class="java.lang.Long" calculation="Sum">
+		<variableExpression><![CDATA[$V{Станция_BooL} && $V{BooL_0}  ?
+( $F{Время возврата на базу}.getTime() - $F{Время регистрации}.getTime() ) / 1000 : null]]></variableExpression>
 	</variable>
  '''
 
@@ -26,8 +26,8 @@ while i < 24:
 #Вторая серия переменных
 
 bas_template = '''
-	<variable name="БригадаКол-во_0" class="java.lang.Integer" calculation="Count">
-		<variableExpression><![CDATA[$V{BooL_0} && $V{Бригада_BooL} ? 1 : null]]></variableExpression>
+	<variable name="СтанцияКол-во_0" class="java.lang.Integer" calculation="Count">
+		<variableExpression><![CDATA[$V{BooL_0} && $V{Станция_BooL} ? 1 : null]]></variableExpression>
 	</variable>
  '''
 
@@ -44,8 +44,8 @@ while i < 24:
 #Третяя серия переменных
 
 bas_template = '''
-	<variable name="Бригада_0" class="java.lang.Long">
-		<variableExpression><![CDATA[$V{БригадаСумВремя(sec)_0}/$V{БригадаКол-во_0}]]></variableExpression>
+	<variable name="Станция_0" class="java.lang.Long">
+		<variableExpression><![CDATA[$V{СтанцияСумВремя(sec)_0}/$V{СтанцияКол-во_0}]]></variableExpression>
 	</variable>
  '''
 
