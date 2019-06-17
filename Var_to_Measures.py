5fbe8657f4dd8b4b8a56bfbd33121cad0d41082c
Alexandr <alexandr.baryshev@hotmail.com>
2019-06-17 17:40:21 +1000

    Save

--- Var_to_Measures.py
+++ Var_to_Measures.py
@@ -11,24 +11,12 @@ bas_template = '''
 
 template = bas_template.replace("{", "{{").replace("}", "}}").replace("variable", "{0}")
 
-template = re.sub(r'class=".+?"', 'class="{1}', template)
+template = re.sub(r'class=".+?"', 'class="{1}"', template)
 
-gotovo = template.format("measure", "lang.Object")
+gotovo = template.format("measure", "java.lang.Object")
 
 print(gotovo)
 
 clipboard.copy(gotovo)
 
 
-'''
-<variable name="ПередачВызСумВрем" class="java.lang.Long" calculation="Sum">
-<variableExpression><![CDATA[$F{Время регистрации} != null && $F{Время получения команды на выезд}  != null ?
-( $F{Время получения команды на выезд}.getTime() - $F{Время регистрации}.getTime() ) / 1000 : null]]></variableExpression>
-</variable>
-
-
-<measure name="ПередачВызСумВрем" class="java.lang.Object" calculation="Sum">
-<measureExpression><![CDATA[$F{Время регистрации} != null && $F{Время получения команды на выезд}  != null ?
-( $F{Время получения команды на выезд}.getTime() - $F{Время регистрации}.getTime() ) / 1000 : null]]></measureExpression>
-</measure>
-'''
