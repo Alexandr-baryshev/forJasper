0b793d11c4caea035647420172bfc991dc687259
Alexandr <alexandr.baryshev@hotmail.com>
2019-08-06 16:16:17 +1000

    Save

--- Generator/Generator.py
+++ Generator/Generator.py
@@ -1,6 +1,4 @@
-import re
 import uuid
-import clipboard
 import xml.etree.ElementTree as Etree
 
 tree = Etree.parse('GenJasperTemplate.jrxml')
@@ -10,21 +8,12 @@ print(root)
 for child in root:
     print(child.tag)
     if '{http://jasperreports.sourceforge.net/jasperreports}summary' == child.tag:
-        # print(child.attrib)
-        # child.set('example', 'vl')
-        # print(child.attrib)
+
         b = child.find('{http://jasperreports.sourceforge.net/jasperreports}band')
 
-        # < textField >
         tf = Etree.Element("{http://jasperreports.sourceforge.net/jasperreports}textField")
         b.insert(1, tf)
 
-        # < reportElement
-        # x = "90"
-        # y = "50"
-        # width = "100"
-        # height = "30"
-        # uuid = "923859d3-ad08-4ba1-b4cc-816c28228d14" / >
         re = Etree.Element("{http://jasperreports.sourceforge.net/jasperreports}reportElement")
         re.set("x", "90")
         re.set("y", "50")
@@ -33,13 +22,9 @@ for child in root:
         re.set("uuid", str(uuid.uuid4()))
         tf.insert(1, re)
 
-        # < textFieldExpression > <![CDATA["Text Field"]] > < / textFieldExpression >
         tfe = Etree.Element("{http://jasperreports.sourceforge.net/jasperreports}textFieldExpression")
         tfe.text = '"ddd"'
         tf.insert(1, tfe)
 
 
-
 tree.write('GenJasper.jrxml')
-
-
--- Var_and_Field/Var_x24.py
+++ Var_and_Field/Var_x24.py
@@ -2,17 +2,16 @@ import clipboard
 
 
 bas_template = '''
-	<variable name="ГоспитСумВремя(sec)_0" class="java.lang.Long" calculation="Sum">
-		<variableExpression><![CDATA[$V{Госпит_BooL} && $V{BooL_0}  ?
-( $F{Время убытия с места происшествия}.getTime() - $F{Время окончания транспортировки}.getTime()  ) / 1000 : null]]></variableExpression>
+	<variable name="R1*C1" class="java.lang.Integer" calculation="DistinctCount">
+		<variableExpression><![CDATA[$V{R1} && $V{C1} ? $F{Идентификатор происшествия} : null]]></variableExpression>
 	</variable>
  '''
 
-template = bas_template.replace("{", "{{").replace("}", "}}").replace("_0", "_{0}")
+template = bas_template.replace("{", "{{").replace("}", "}}").replace("R1", "R{0}")
 
 all_gotovo = ""
-i = 0
-while i < 24:
+i = 1
+while i < 9:
     gotovo = template.format(i)
     all_gotovo = all_gotovo + gotovo + "\r\n"
     print(gotovo)
