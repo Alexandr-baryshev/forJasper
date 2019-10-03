6cf546b3eaf99ce2d3197ee00a5068859b626238
Alexandr <alexandr.baryshev@hotmail.com>
2019-10-03 15:53:41 +1000

    Save

--- Var_and_Field/Field_RC.py
+++ Var_and_Field/Field_RC.py
@@ -5,28 +5,40 @@ import re
 # --- ПОЛЕ ----------------------------------------------------------------------
 field = '''
 			<textField isStretchWithOverflow="true" isBlankWhenNull="true">
-				<reportElement style="Ячейка" stretchType="RelativeToBandHeight" x="145" y="215" width="20" height="20" uuid="d7394213-df75-4799-900b-c14998306aac"/>
+				<reportElement style="Ячейка" stretchType="RelativeToBandHeight" x="145" y="20" width="20" height="20" uuid="d7394213-df75-4799-900b-c14998306aac"/>
 				<textElement>
 					<font isBold="true"/>
 				</textElement>
-				<textFieldExpression><![CDATA[$V{R11 & C1}]]></textFieldExpression>
+				<textFieldExpression><![CDATA[$V{R1 & C1}]]></textFieldExpression>
 			</textField>
  '''
 
-
-columnAndX = field.replace("{", "{{").replace("}", "}}").replace("C1", "C{0}").replace('x="145"', 'x="{1}"')
-
-columnAndX = re.sub(r'uuid=".+?"', 'uuid="{2}"', columnAndX)
-
 allResult = ""
+# --- R СТРОКА замена 11 раз ----------------------->
+rowAndY = field.replace("{", "{{").replace("}", "}}").replace("R1", "R{0}").replace('Y="20"', 'Y="{1}"')
+rowAndY = re.sub(r'uuid=".+?"', 'uuid="{2}"', rowAndY)
+ry = 1
+while ry <= 11:
+    y = 20 * ry + 20
+    rowAndYResult = rowAndY.format(ry, y, str(uuid.uuid4()),)
 
-cx = 1
-while cx <= 31:
-    x = 20 * cx + 125
-    columnAndXResult = columnAndX.format(cx, x, str(uuid.uuid4()),)
-    allResult = allResult + columnAndXResult + "\r\n"
-    print(columnAndXResult)
-    cx = cx + 1
+    # --- C СТОЛБЕЦ замена 31 раз ->
+    columnAndX = rowAndYResult.replace("{", "{{").replace("}", "}}").replace("C1", "C{0}").replace('x="145"', 'x="{1}"')
+    columnAndX = re.sub(r'uuid=".+?"', 'uuid="{2}"', columnAndX)
+    cx = 1
+    while cx <= 31:
+        x = 20 * cx + 125
+        columnAndXResult = columnAndX.format(cx, x, str(uuid.uuid4()),)
+
+        allResult = allResult + columnAndXResult + "\r\n"
+        print(columnAndXResult)
+        cx = cx + 1
+        # --- СТОЛБЕЦ конец - ///
+
+
+    ry = ry + 1
+
+# --- СТРОКА конец ------------------------------ ///
 
 clipboard.copy(allResult)
 
