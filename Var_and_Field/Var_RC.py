b6615d69134de065451a8a47662d3c63604d9835
Alexandr <alexandr.baryshev@hotmail.com>
2019-10-03 14:52:33 +1000

    Save

--- Var_and_Field/Var_RC.py
+++ Var_and_Field/Var_RC.py
@@ -1,27 +1,38 @@
 import clipboard
 
-# ---Замена R ----------------------------------------------------------------------------------------
+# --- ПЕРЕМЕННАЯ ----------------------------------------------------------------------
 
 variable = '''
 	<variable name="R1 &amp; C1" class="java.lang.Integer" calculation="DistinctCount">
 		<variableExpression><![CDATA[$V{R1} && $V{C1} ? $F{Идентификатор происшествия} : null]]></variableExpression>
 	</variable>
  '''
+# -----------------
 
-row = variable.replace("{", "{{").replace("}", "}}").replace("C1", "C{0}").replace("R1", "R{0}")
-rowALL = ""
+allResult = ""
 
 
+# --- R СТРОКА замена 11 раз ----------------------->
+row = variable.replace("{", "{{").replace("}", "}}").replace("R1", "R{0}")
+r = 1
+while r <= 11:
+    rowResult = row.format(r)
 
-# ---подцикл / НАЧАЛО -- Замена C при постоянной R ------------------------------------------------------------------
-i = 1
-while i <= 31:
-    rowResult = row.format(i)
-    rowALL = rowALL + rowResult + "\r\n"
-    print(rowResult)
-    i = i + 1
-# ---подцикл / КОНЕЦ -- Замена C при постоянной R ------------------------------------------------------------------
+    # --- C СТОЛБЕЦ замена 31 раз ->
+    column = rowResult.replace("{", "{{").replace("}", "}}").replace("C1", "C{0}")
+    c = 1
+    while c <= 31:
+        columnResult = column.format(c)
+        allResult = allResult + columnResult + "\r\n"
+        print(columnResult)
+        c = c + 1
+    # --- СТОЛБЕЦ конец - ///
+
+    r = r + 1
+# --- СТРОКА конец ------------------------------ ///
+
+
 
 
 # --- КОПИЯ ВСЕГО
-clipboard.copy(rowALL)
+clipboard.copy(allResult)
