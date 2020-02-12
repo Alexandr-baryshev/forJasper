cc69cc1af8095634a41171dcd7091c9efe606e1d
Alexandr <alexandr.baryshev@hotmail.com>
2020-02-12 14:00:30 +1000

    Save

--- Var_and_Field/Jasper to Jason.py
+++ Var_and_Field/Jasper to Jason.py
@@ -19,7 +19,7 @@ jason = [
         "id": "id_ce_TIME_REG_a73b8922-00d0-45d6-83c1-e48031587d02",
         "idField": "id_ce_TIME_REG"
     },
-            '''
+   '''
     ,
     '''
     {
@@ -30,7 +30,7 @@ jason = [
         "id": "id_ce_CALL_NUMB_750fdc32-780a-4e08-974e-1261b6d4fef5",
         "idField": "id_ce_CALL_NUMB"
     },
-            '''
+    '''
     ,
     '''
     {
@@ -41,7 +41,7 @@ jason = [
         "id": "id_ce_RETREAT_REASON_f1aa8917-7df8-4d26-8cc9-a2122b7c31df",
         "idField": "id_ce_RETREAT_REASON"
     },
-            '''
+     '''
     ,
     '''
     {
@@ -52,7 +52,7 @@ jason = [
         "id": "id_ce_RANK_4e68b53d-23b6-4fab-88d0-a3e19ee03546",
         "idField": "id_ce_RANK"
     },
-            '''
+     '''
     ,
     '''
     {
@@ -63,7 +63,7 @@ jason = [
         "id": "id_addr_ADRESS_b9d38ea2-a110-473f-9340-de02cbc0564d",
         "idField": "id_addr_ADRESS"
     },
-            '''
+      '''
     ,
     '''
     {
@@ -74,7 +74,7 @@ jason = [
         "id": "e_ct_b_caption_97561bdf-bff2-4ace-a5e6-466437dc5523",
         "idField": "e_ct_b_caption"
     },
-            '''
+    '''
     ,
     '''
     {
@@ -85,7 +85,7 @@ jason = [
         "id": "e_ct_b_tt_9a5a832e-98ae-4020-aff0-5a4f2fdc74ae",
         "idField": "e_ct_b_tt"
     },
-            '''
+    '''
     ,
     '''
     {
@@ -96,8 +96,7 @@ jason = [
         "id": "e_ct_b_place_time_aa9fc7ee-9cb3-438a-a868-ab301fb9a3ca",
         "idField": "e_ct_b_place_time"
     },
-
-        '''
+    '''
     ,
     '''
     {
@@ -108,7 +107,7 @@ jason = [
         "id": "e_ct_b_return_time_f4f0da30-d558-4b17-b670-26bf8b7d5e05",
         "idField": "e_ct_b_return_time"
     },
-            '''
+     '''
     ,
     '''
     {
@@ -119,8 +118,7 @@ jason = [
         "id": "e_ct_b_receive_time_712eaae3-197d-46e9-ac0a-da3390797e27",
         "idField": "e_ct_b_receive_time"
     },
-
-            '''
+    '''
     ,
     '''
     {
@@ -131,7 +129,7 @@ jason = [
         "id": "e_ct_b_retreat_time_5cc056eb-6a96-46ed-87e4-5cbbb27321f8",
         "idField": "e_ct_b_retreat_time"
     },
-            '''
+    '''
     ,
     '''
     {
@@ -142,7 +140,7 @@ jason = [
         "id": "e_ct_b_to_hospital_32bf7564-50fb-49c9-ab7c-8e83323e731d",
         "idField": "e_ct_b_to_hospital"
     },
-            '''
+     '''
     ,
     '''
     {
@@ -153,7 +151,7 @@ jason = [
         "id": "e_ct_b_at_hospital_29e4ddbb-c264-41a8-8478-c018b4533df9",
         "idField": "e_ct_b_at_hospital"
     },
-            '''
+    '''
     ,
     '''
     {
@@ -164,7 +162,7 @@ jason = [
         "id": "card03_p_20c0e8f5-2beb-4f34-86d6-591ede67d816",
         "idField": "card03_p"
     },
-            '''
+     '''
     ,
     '''
     {
@@ -175,8 +173,7 @@ jason = [
         "id": "card03_age_p_9fa52a21-40ad-4294-8d03-ef46c46c8913",
         "idField": "card03_age_p"
     },
-
-            '''
+'''
     ,
     '''
     {
@@ -187,7 +184,7 @@ jason = [
         "id": "card03_icd10_primary_cod_ef8c477d-4294-47ad-835b-f283d66d8a3b",
         "idField": "card03_icd10_primary_cod"
     },
-            '''
+    '''
     ,
     '''
     {
@@ -198,7 +195,7 @@ jason = [
         "id": "card03_icd10_primary_cap_5ab005d3-110d-4220-9f3d-c71aa76d9cdb",
         "idField": "card03_icd10_primary_cap"
     },
-            '''
+      '''
     ,
     '''
     {
@@ -209,7 +206,7 @@ jason = [
         "id": "card03_icd10_hos_dia_cod_c42d3f12-abd4-460e-aa9d-5f203f27e5da",
         "idField": "card03_icd10_hos_dia_cod"
     },
-            '''
+    '''
     ,
     '''
     {
@@ -220,7 +217,7 @@ jason = [
         "id": "card03_icd10_hos_dia_cap_289d816b-5b99-4374-a6c7-1cf09517cc1b",
         "idField": "card03_icd10_hos_dia_cap"
     },
-            '''
+      '''
     ,
     '''
     {
@@ -231,7 +228,7 @@ jason = [
         "id": "card03_icd10_acc_cod_ceec7604-302d-4e0c-bbfa-1c7085c3b57e",
         "idField": "card03_icd10_acc_cod"
     },
-            '''
+     '''
     ,
     '''
     {
@@ -242,7 +239,7 @@ jason = [
         "id": "card03_icd10_acc_cap_88f595bb-1f70-4d4b-9fe2-4c52ad34883c",
         "idField": "card03_icd10_acc_cap"
     },
-            '''
+    '''
     ,
     '''
     {
@@ -253,7 +250,7 @@ jason = [
         "id": "card03_icd10_final_cod_592b9fac-3b85-426d-8c55-cbbe32ef6a8e",
         "idField": "card03_icd10_final_cod"
     },
-            '''
+      '''
     ,
     '''
     {
@@ -264,7 +261,7 @@ jason = [
         "id": "card03_icd10_final_cap_f3c170d0-3247-477b-a64f-0f8b98a9c09b",
         "idField": "card03_icd10_final_cap"
     },
-            '''
+     '''
     ,
     '''
     {
@@ -275,8 +272,7 @@ jason = [
         "id": "c03_hospital_b4eb717e-d09f-4932-a379-0fbb83030a5a",
         "idField": "c03_hospital"
     },
-
-            '''
+ '''
     ,
     '''
     {
@@ -287,8 +283,7 @@ jason = [
         "id": "c03_pat_sex_d_1b852030-68a4-4c88-b14c-65f22575cb6c",
         "idField": "c03_pat_sex_d"
     },
-
-            '''
+'''
     ,
     '''
     {
@@ -299,7 +294,7 @@ jason = [
         "id": "c03_pat_reg_place_a10458d6-186c-4a9c-80ed-f3f09a47a3e8",
         "idField": "c03_pat_reg_place"
     },
-            '''
+     '''
     ,
     '''
     {
@@ -310,7 +305,7 @@ jason = [
         "id": "c03_category_call_res_d_72a0e59e-b90e-4a95-8915-b6fbba08e6e4",
         "idField": "c03_category_call_res_d"
     },
-            '''
+     '''
     ,
     '''
     {
@@ -361,4 +356,5 @@ while len(jasper) - 1 > b:  # ЦИКЛ СМЕНЫ b
             result.append(jasper[b] + " < Не найдено")
             print("Поле не совпало, a+1")
 
-print(result)
+for i in result:
+    print(i)
