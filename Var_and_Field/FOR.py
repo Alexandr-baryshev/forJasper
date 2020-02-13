101b7df1c1ad819cff71b172d5dff80a637bafb5
Alexandr <alexandr.baryshev@hotmail.com>
2020-02-13 14:19:41 +1000

    Save

--- Var_and_Field/FOR.py
+++ Var_and_Field/FOR.py
@@ -5,6 +5,8 @@ with open('Zapros.json', encoding='UTF8') as f:
 jason = jason_all['Zapros']
 
 jasper = [
+    "Номер вызова",
+    "Идентификатор происшествия",
     "Техника Позывной",
     "ФИО пациента",
     "ЛПУ",
@@ -14,30 +16,27 @@ jasper = [
     "Характер вызова по результату",
     "Возраст",
     "Адрес",
-    "Номер вызова",
-    "Идентификатор происшествия",
     "Заявитель",
     "Время регистрации",
     "Бригада Позывной"
 ]
+
 result = []
 not_find = []
 
 b = -1
-while len(jasper) - 1 > b:  # ЦИКЛ СМЕНЫ b
-    b = b + 1
-    a = -1
-    res = False
-    while len(jason) - 1 > a:
-        a = a + 1
-        if jason[a]['caption'] == jasper[b]:
-            result.append(jason[a])
-            res = True
-            print("Поле совпало, b+1")
+a = -1
 
-        if len(jason) - 1 == a and not res:
-            not_find.append(jasper[b] + " < Не найдено")
-            print("Поле не совпало, a+1")
+for z in jasper:
+    b = b + 1
+    for x in jason:
+        a = a + 1
+
+    if jason[a]['caption'] == jasper[b]:
+        result.append(jason[a])
+
+    if len(jason) - 1 == a:
+        not_find.append(jasper[b] + " < Не найдено")
 
 
 
@@ -45,15 +44,6 @@ print("----------------------")
 print(json.dumps(result, ensure_ascii=False, indent=4))
 print("----------------------")
 
-# ff = 0
-# while len(not_find) > ff:
-#     i = not_find[ff]
-#     print(i)
-#     ff = ff + 1
-
-
 print("???????????????????????")
 for i in not_find:
     print(i)
-
-
