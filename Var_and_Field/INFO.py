bb01bec1d6f61e9b6b06b9abd1285818ffc62b6d
Alexandr <alexandr.baryshev@hotmail.com>
2020-02-19 16:21:30 +1000

    Save

--- Var_and_Field/INFO.py
+++ Var_and_Field/INFO.py
@@ -1,20 +1,20 @@
 import builtins
 
+
 a = 100
 
 # ВСЕ АНАЛОГИЧНО
 
 a.__add__(5)
-
 builtins.int.__add__(100, 5)
 
 
-
 x = builtins.int.__add__
-
 x(100, 5)
 
 
+y = builtins.int
+y.__add__(100, 5)
 
 
 print()
