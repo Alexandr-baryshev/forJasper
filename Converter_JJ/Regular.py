cb59b9c683e03315b987e69d7bf31c7926dff586
Alexandr <alexandr.baryshev@hotmail.com>
2020-02-20 12:51:37 +1000

    Save

--- Converter_JJ/Regular.py
+++ Converter_JJ/Regular.py
@@ -1,25 +1,67 @@
 import re
 
 xml_string = '''
-	<field name="Дней" class="java.lang.Number">
+	<field name="Старший бригады" class="java.lang.String">
 		<fieldDescription><![CDATA[]]></fieldDescription>
 	</field>
-	<field name="Месяцев" class="java.lang.Number">
+	<field name="Госпитализирован" class="java.lang.Number">
 		<fieldDescription><![CDATA[]]></fieldDescription>
 	</field>
-	<field name="Место регистрации" class="java.lang.String">
+	<field name="ФИО пациента" class="java.lang.String">
 		<fieldDescription><![CDATA[]]></fieldDescription>
 	</field>
-	<field name="Характер вызова по результату" class="java.lang.String">
+	<field name="ЛПУ" class="java.lang.String">
 		<fieldDescription><![CDATA[]]></fieldDescription>
 	</field>
-	<field name="Пол пациента код" class="java.lang.Number">
+	<field name="Расшифровка (ОД)" class="java.lang.String">
 		<fieldDescription><![CDATA[]]></fieldDescription>
 	</field>
-	<field name="Лет" class="java.lang.Number">
+	<field name="Результат вызова" class="java.lang.String">
 		<fieldDescription><![CDATA[]]></fieldDescription>
 	</field>
-	<field name="Идентификатор происшествия" class="java.lang.Number">
+	<field name="Возраст" class="java.lang.String">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Адрес" class="java.lang.String">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Номер вызова" class="java.lang.Number">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Содержание текста происшествия" class="java.lang.String">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Регистрационный номер" class="java.lang.Number">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Время регистрации" class="java.util.Date">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Время возврата на базу" class="java.util.Date">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Время прибытия на место происшествия" class="java.util.Date">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Время получения команды на выезд" class="java.util.Date">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Время выезда" class="java.util.Date">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Повод" class="java.lang.String">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Наименование типа сил и средств" class="java.lang.String">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Код (ОД)" class="java.lang.String">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Время убытия с места происшествия" class="java.util.Date">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Идентификатор происшествия" class="java.lang.Integer">
 		<fieldDescription><![CDATA[]]></fieldDescription>
 	</field>
 '''
