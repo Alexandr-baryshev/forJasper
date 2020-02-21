9e77821ff5cacf601d38a00e98d698630a9aafa9
Alexandr <alexandr.baryshev@hotmail.com>
2020-02-21 16:05:43 +1000

    Save

--- Converter_JJ/Jasper_To_Jason.py
+++ Converter_JJ/Jasper_To_Jason.py
@@ -10,6 +10,12 @@ with open('Zapros.json', encoding='UTF8') as f:
     jason_all = json.load(f)
 jason = jason_all['Zapros']
 
+
+with open('Zapros_past.json', encoding='UTF8') as ff:
+    zapros_past = json.load(ff)
+zapros = zapros_past['columns']
+
+
 jasper = Regular.result
 
 result = []
@@ -35,7 +41,7 @@ if len(not_find) > 0:
     ColorModule.print_yellow('ПОЛЯ, НЕ НАЙДЕНЫЕ В ФАЙЛЕ "Zapros.json"')
 
     for i in not_find:
-        ColorModule.print_red('i')
+        ColorModule.print_red(i)
 
 if len(not_find) < 1:
     ColorModule.print_green('ВСЕ ПОЛЯ НАЙДЕНЫ')
--- Converter_JJ/Regular.py
+++ Converter_JJ/Regular.py
@@ -1,66 +1,31 @@
 import re
 
 xml_string = '''
-	<field name="Старший бригады" class="java.lang.String">
-		<fieldDescription><![CDATA[]]></fieldDescription>
-	</field>
-	<field name="Госпитализирован" class="java.lang.Number">
-		<fieldDescription><![CDATA[]]></fieldDescription>
-	</field>
-	<field name="ФИО пациента" class="java.lang.String">
-		<fieldDescription><![CDATA[]]></fieldDescription>
-	</field>
 	<field name="ЛПУ" class="java.lang.String">
-		<fieldDescription><![CDATA[]]></fieldDescription>
-	</field>
-	<field name="Расшифровка (ОД)" class="java.lang.String">
-		<fieldDescription><![CDATA[]]></fieldDescription>
-	</field>
-	<field name="Результат вызова" class="java.lang.String">
-		<fieldDescription><![CDATA[]]></fieldDescription>
-	</field>
-	<field name="Возраст" class="java.lang.String">
-		<fieldDescription><![CDATA[]]></fieldDescription>
-	</field>
-	<field name="Адрес" class="java.lang.String">
-		<fieldDescription><![CDATA[]]></fieldDescription>
-	</field>
-	<field name="Номер вызова" class="java.lang.Number">
-		<fieldDescription><![CDATA[]]></fieldDescription>
-	</field>
-	<field name="Содержание текста происшествия" class="java.lang.String">
-		<fieldDescription><![CDATA[]]></fieldDescription>
-	</field>
-	<field name="Регистрационный номер" class="java.lang.Number">
-		<fieldDescription><![CDATA[]]></fieldDescription>
-	</field>
-	<field name="Время регистрации" class="java.util.Date">
-		<fieldDescription><![CDATA[]]></fieldDescription>
-	</field>
-	<field name="Время возврата на базу" class="java.util.Date">
-		<fieldDescription><![CDATA[]]></fieldDescription>
-	</field>
-	<field name="Время прибытия на место происшествия" class="java.util.Date">
-		<fieldDescription><![CDATA[]]></fieldDescription>
-	</field>
-	<field name="Время получения команды на выезд" class="java.util.Date">
-		<fieldDescription><![CDATA[]]></fieldDescription>
-	</field>
-	<field name="Время выезда" class="java.util.Date">
-		<fieldDescription><![CDATA[]]></fieldDescription>
-	</field>
-	<field name="Повод" class="java.lang.String">
-		<fieldDescription><![CDATA[]]></fieldDescription>
-	</field>
-	<field name="Наименование типа сил и средств" class="java.lang.String">
 		<fieldDescription><![CDATA[]]></fieldDescription>
 	</field>
 	<field name="Код (ОД)" class="java.lang.String">
 		<fieldDescription><![CDATA[]]></fieldDescription>
 	</field>
-	<field name="Время убытия с места происшествия" class="java.util.Date">
+	<field name="Код (ПО)" class="java.lang.String">
 		<fieldDescription><![CDATA[]]></fieldDescription>
 	</field>
+	<field name="Номер вызова" class="java.lang.Number">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Время регистрации" class="java.util.Date">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Причины отказа в госпитализации" class="java.lang.String">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Блок (ОД)" class="java.lang.String">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Блок (ПО)" class="java.lang.String">
+		<fieldDescription><![CDATA[]]></fieldDescription>
+	</field>
+	<field name="Старший бригады" class="java.lang.String"/>
 	<field name="Идентификатор происшествия" class="java.lang.Integer">
 		<fieldDescription><![CDATA[]]></fieldDescription>
 	</field>
