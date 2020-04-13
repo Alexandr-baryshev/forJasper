46e92346cc362009e27e9e2ce4c85adbe3cf67f9
alexandr_baryshev <alexandr.baryshev@hotmail.com>
2020-04-13 22:58:09 +1000

    Save

--- Var_and_Field/Var_CR.py
+++ Var_and_Field/Var_CR.py
@@ -1,4 +1,5 @@
 import pyperclip
+
 # ФОРМИРОВАНИ ПО ПРИНЦИПУ - C1 R1,  C1 R2,  C1 R3, и тд. * простое выделение столбцов в редакторе
 # --- ПЕРЕМЕННАЯ ----------------------------------------------------------------------
 
@@ -7,31 +8,34 @@ variable = '''
 		<variableExpression><![CDATA[$V{R1} ? $V{C1} : null]]></variableExpression>
 	</variable>
  '''
-# -------------------------------------------------------------------
 
-allResult = ""
 
 # --- C СТОЛБЕЦ замена X раз ----------------------->
-row = variable.replace("{", "{{").replace("}", "}}").replace("C1", "C{0}")
-c = 1
-while c <= 1:
-    rowResult = row.format(c)
 
-    # --- R СТРОКА замена X раз ->
-    column = rowResult.replace("{", "{{").replace("}", "}}").replace("R1", "R{0}")
-    r = 1
-    while r <= 10:
-        columnResult = column.format(r)
-        allResult = allResult + columnResult + "\r\n"
-        print(columnResult)
-        r = r + 1
-    # --- СТОЛБЕЦ конец - ///
 
-    c = c + 1
+def Cx_Rx(C_Start, C_Max, R_Start, R_Max):
+    allResult = ""
+    row = variable.replace("{", "{{").replace("}", "}}").replace("C1", "C{0}")
+    c = C_Start
+    while c <= C_Max:
+        rowResult = row.format(c)
+
+        # --- R СТРОКА замена X раз ->
+        column = rowResult.replace("{", "{{").replace("}", "}}").replace("R1", "R{0}")
+        r = R_Start
+        while r <= R_Max:
+            columnResult = column.format(r)
+            allResult = allResult + columnResult + "\r\n"
+            # print(columnResult)
+            r = r + 1
+        # --- СТОЛБЕЦ конец - ///
+
+        c = c + 1
+    pyperclip.copy(allResult)
+    return allResult
+
+
 # --- СТРОКА конец ------------------------------ ///
 
+print(Cx_Rx(1, 3, 1, 3))
 
-
-
-# --- КОПИЯ ВСЕГО
-pyperclip.copy(allResult)
