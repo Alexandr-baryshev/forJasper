3c5a926114fbbc60553b202267afad279de5e0d4
alexandr_baryshev <alexandr.baryshev@hotmail.com>
2020-04-14 00:06:19 +1000

    Save

--- Var_and_Field/Var_CR.py
+++ Var_and_Field/Var_CR.py
@@ -3,7 +3,14 @@ import pyperclip
 # ФОРМИРОВАНИ ПО ПРИНЦИПУ - C1 R1,  C1 R2,  C1 R3, и тд. * простое выделение столбцов в редакторе
 # --- ПЕРЕМЕННАЯ ----------------------------------------------------------------------
 
-variable = '''
+q = input("Встаить содержимое из буфера? Y / N" + "\n")
+
+if q == "Y" or q == "y":
+    variable = pyperclip.paste()
+else:
+    exit()
+
+variable22 = '''
 	<variable name="R1 C1" class="java.lang.Integer" calculation="Sum">
 		<variableExpression><![CDATA[$V{R1} ? $V{C1} : null]]></variableExpression>
 	</variable>
@@ -13,17 +20,19 @@ variable = '''
 # --- C СТОЛБЕЦ замена X раз ----------------------->
 
 
-def Cx_Rx(C_Start, C_Max, R_Start, R_Max):
+def Cx_Rx(C_Start=int(input("Старт колонки от - ")), C_Size=int(input("Размер клонки - ")),
+          R_Start=int(input("Старт строки от - ")), R_Size=int(input("Размер строки - "))):
+
     allResult = ""
     row = variable.replace("{", "{{").replace("}", "}}").replace("C1", "C{0}")
     c = C_Start
-    while c <= C_Max:
+    while c <= C_Size:
         rowResult = row.format(c)
 
         # --- R СТРОКА замена X раз ->
         column = rowResult.replace("{", "{{").replace("}", "}}").replace("R1", "R{0}")
         r = R_Start
-        while r <= R_Max:
+        while r <= R_Size:
             columnResult = column.format(r)
             allResult = allResult + columnResult + "\r\n"
             # print(columnResult)
@@ -37,5 +46,6 @@ def Cx_Rx(C_Start, C_Max, R_Start, R_Max):
 
 # --- СТРОКА конец ------------------------------ ///
 
-print(Cx_Rx(1, 3, 1, 3))
+Cx_Rx()
 
+print(Cx_Rx())
